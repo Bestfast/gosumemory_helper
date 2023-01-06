6e6e26cb9a31197cdd88d988629a559210dd65e0
Bestfast <bestfast03@gmail.com>
2023-01-06 09:58:02 +0100

    Force structs to be initialized by new or default

--- src/lib.rs
+++ src/lib.rs
@@ -1,71 +1,91 @@
-use json;
-use tungstenite::Message;
+use json::JsonValue;
 
-struct Metadata {
-    title: String,
-    diff: String,
-    artist: String,
-    mapper: String,
+#[derive(Clone, PartialEq, Default)]
+pub struct Metadata {
+    pub title: String,
+    pub diff: String,
+    pub artist: String,
+    pub mapper: String,
+    _private: (),
 }
 
-struct Stats {
+impl Metadata {
+    pub fn new(msg: &JsonValue) -> Result<Metadata, &'static str> {
+        // metadata
+        let metadata_json = &msg["menu"]["bm"]["metadata"];
+        Ok(Metadata {
+            title: metadata_json["title"].to_string(),
+            diff: metadata_json["difficulty"].to_string(),
+            artist: metadata_json["artist"].to_string(),
+            mapper: metadata_json["mapper"].to_string(),
+            _private: ()
+        })
+    }
+}
+
+#[derive(Clone, PartialEq, Default)]
+pub struct Stats {
     od: f32,
     hp: f32,
     sr: f32,
     bpmmin: f32,
     bpmmax: f32,
+    _private: (),
 }
 
-struct Path {
-    folder: String,
-    file: String,
-    bg: String,
-    audio: String,
-}
-
-struct OsuMap {
-    metadata: Metadata,
-    stats: Stats,
-    path: Path,
-}
-
-impl OsuMap {
-    fn new(msg: Message) -> Result<OsuMap, &'static str> {
-        let text = msg.to_text().unwrap();
-        let text_json = json::parse(text).unwrap();
-
-        // metadata
-        let metadata_json = &text_json["menu"]["bm"]["metadata"];
-        let metadata = Metadata {
-            title: metadata_json["title"].to_string(),
-            diff: metadata_json["difficulty"].to_string(),
-            artist: metadata_json["artist"].to_string(),
-            mapper: metadata_json["mapper"].to_string(),
-        };
-
+impl Stats {
+    pub fn new(msg: &JsonValue) -> Result<Stats, &'static str> {
         // stats
-        let stats_json = &text_json["menu"]["bm"]["stats"];
-        let stats = Stats {
+        let stats_json = &msg["menu"]["bm"]["stats"];
+        Ok(Stats {
             od: stats_json["memoryOD"].as_f32().unwrap(),
             hp: stats_json["memoryHP"].as_f32().unwrap(),
             sr: stats_json["fullSR"].as_f32().unwrap(),
             bpmmin: stats_json["BPM"]["min"].as_f32().unwrap(),
             bpmmax: stats_json["BPM"]["max"].as_f32().unwrap(),
-        };
+            _private: (),
+        })
+    }
+}
 
+#[derive(Clone, PartialEq, Default)]
+pub struct Path {
+    folder: String,
+    file: String,
+    bg: String,
+    audio: String,
+    _private: (),
+}
+
+impl Path {
+    pub fn new(msg: &JsonValue) -> Result<Path, &'static str> {
         // path
-        let path_json = &text_json["menu"]["bm"]["path"];
-        let path = Path {
+        let path_json = &msg["menu"]["bm"]["path"];
+        Ok(Path {
             folder: path_json["folder"].to_string(),
             file: path_json["file"].to_string(),
             bg: path_json["bg"].to_string(),
             audio: path_json["audio"].to_string(),
-        };
+            _private: (),
+        })
+    }
+}
 
+#[derive(Clone, PartialEq, Default)]
+pub struct OsuMap {
+    pub metadata: Metadata,
+    pub stats: Stats,
+    pub path: Path,
+    _private: (),
+}
+
+impl OsuMap {
+    pub fn new(msg: &JsonValue) -> Result<OsuMap, &'static str> {
         Ok(OsuMap {
-            metadata,
-            stats,
-            path,
+            metadata: Metadata::new(msg).unwrap(),
+            stats: Stats::new(msg).unwrap(),
+            path: Path::new(msg).unwrap(),
+            _private: (),
         })
     }
 }
